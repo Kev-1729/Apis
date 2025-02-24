a71f6eb95547de43bf7bf72d2d6c8ad90824e3d5
Kevin Tupac Agüero <96688861+Kev-1729@users.noreply.github.com>
2025-02-23 21:47:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -31,11 +31,29 @@ class InputData(BaseModel):
 @app.get("/")
 def home():
     return {
-        "mensaje": "Bienvenido a la API de Machine Learning para predicción de deserción laboral.",
-        "descripcion": "Esta API usa un modelo de Machine Learning para hacer predicciones de renuncias basadas en características de empleados.",
+        "mensaje": "Bienvenido a la API de Machine Learning para predicción de empleados.",
+        "descripcion": "Esta API usa un modelo de Machine Learning para hacer predicciones basadas en características laborales y personales del empleado.",
         "endpoints": {
             "GET /": "Información sobre la API.",
             "POST /predict/": "Enviar características para obtener una predicción.",
+        },
+        "variables": {
+            "Age": "Edad del empleado.",
+            "DailyRate": "Salario diario del empleado.",
+            "DistanceFromHome": "Distancia desde la casa al trabajo en kilómetros.",
+            "HourlyRate": "Pago por hora del empleado.",
+            "MonthlyRate": "Salario mensual del empleado.",
+            "YearsAtCompany": "Años trabajando en la empresa.",
+            "BusinessTravel_Travel_Frequently": "Si el empleado viaja frecuentemente por trabajo (True/False).",
+            "MaritalStatus_Single": "Si el empleado es soltero (True/False).",
+            "OverTime_Yes": "Si el empleado trabaja horas extras (True/False).",
+            "JobRole_Technical": "Si el empleado tiene un rol técnico (True/False).",
+            "JobRole_Management": "Si el empleado tiene un rol de gestión o liderazgo (True/False).",
+            "JobRole_Research": "Si el empleado trabaja en investigación y desarrollo (True/False).",
+            "JobRole_Sales": "Si el empleado trabaja en ventas (True/False).",
+            "JobSeniority_Mid": "Si el empleado tiene un nivel de experiencia intermedio (True/False).",
+            "JobSeniority_Senior": "Si el empleado tiene un nivel de experiencia alto (True/False).",
+            "StockOption_Basic": "Si el empleado tiene beneficios básicos de acciones en la empresa (True/False)."
         },
         "ejemplo_prediccion": {
             "url": "/predict/",
@@ -44,10 +62,11 @@ def home():
                 "features": [42.0, 933, 19, 57, 20366.0, 2, True, False, False, False, False, False, True, False, False, False]
             },
             "respuesta_esperada": {
-                "prediction": [1] 
+                "prediction": [1]
             }
         }
     }
+
 
 
 @app.post("/predict/")
